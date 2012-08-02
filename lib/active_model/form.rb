0918d10075b3c713e5c0576330db2f0874ae2de9
Martin Schürrer <martin@schuerrer.org>
2012-08-02 20:34:41 +0200

    Remove code that's now in its own file

--- lib/active_model/form.rb
+++ lib/active_model/form.rb
@@ -63,24 +63,6 @@ module ActiveModel
       end
     end
 
-    private
-
-    # Stolen from ActiveRecord attribute_assignment.rb
-    # Instantiates objects for all attribute classes that needs more than one constructor parameter. This is done
-    # by calling new on the column type or aggregation type (through composed_of) object with these parameters.
-    # So having the pairs written_on(1) = "2004", written_on(2) = "6", written_on(3) = "24", will instantiate
-    # written_on (a date type) with Date.new("2004", "6", "24"). You can also specify a typecast character in the
-    # parentheses to have the parameters typecasted before they're used in the constructor. Use i for Fixnum,
-    # f for Float, s for String, and a for Array. If all the values for a given attribute are empty, the
-    # attribute will be set to nil.
-    def assign_multiparameter_attributes(pairs)
-      execute_callstack_for_multiparameter_attributes(
-        extract_callstack_for_multiparameter_attributes(pairs)
-      )
-    end
-
-
-
     protected
 
     def self.type_from_type_name(type_name)
